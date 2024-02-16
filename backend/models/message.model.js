f189b20cf3bc0b9ad20caa472341b8742e2904e2
Alexis Thierry <alexist103@gmail.com>
2024-02-17 00:24:27 +0100

    [FIX] Changing timestamp type to Date

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -4,7 +4,7 @@ const messageSchema = mongoose.Schema({
     sender_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     receiver_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     content: { type: String, required: true, unique: true },
-    timestamp: { type: Number, required: true }
+    timestamp: { type: Date, required: true }
 });
 
 module.exports = mongoose.model('Message', messageSchema);
