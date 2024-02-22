56d916cc93f9c84e9f174275b885c6584eb5c400
Alexis Thierry <alexist103@gmail.com>
2024-02-22 09:39:50 +0100

    [FEATURE] adding nb_connection field

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -5,6 +5,8 @@ const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     profile_picture: { type: String},
+    nb_connection: { type: Number },
+
 });
 
 module.exports = mongoose.model('User', userSchema);
