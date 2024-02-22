7a4f9d2564021b17cd901e65930b944745fc9e77
Alexis Thierry <alexist103@gmail.com>
2024-02-22 09:40:50 +0100

    [FEATURE] incrementing nb_connection on login

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,22 +5,27 @@ const bcrypt = require('bcrypt');
 exports.getUsers = (req, res) => {
     User.find()
         .then(users => res.status(201).json(users))
-        .catch(error => res.status(401).json({ error }))
+        .catch(error => res.status(401).json({error}))
 }
 
 
-exports.checkUserCredential = (req, res) => {
+exports.login = (req, res) => {
     User.findOne({email: req.body.email})
         .then(user => {
             if (user) {
                 bcrypt.compare(req.body.password, user.password)
                     .then(valid => {
                         if (valid) {
-                            res.status(201).json(true);
+                            User.updateOne({email: req.body.email}, {$inc: {nb_connection: 1}})
+                                .then(() => {
+                                    res.status(201).json(user);
+                                })
+                                .catch(error => res.status(401).json({error}))
                         } else {
                             res.status(201).json(false);
                         }
                     })
+                    .catch(error => req.status(401).json({error}));
             } else {
                 res.status(201).json(false);
             }
@@ -38,12 +43,13 @@ exports.addUser = (req, res) => {
                     username: req.body.username,
                     email: req.body.email,
                     password: hash,
-                    profile_picture: req.body.profile_picture
+                    profile_picture: req.body.profile_picture,
+                    nb_connection: 0
                 });
 
                 user.save()
-                    .then(() => res.status(201).json({ message: 'user_added'}))
-                    .catch(error => res.status(401).json({ error }));
+                    .then(() => res.status(201).json({message: 'user_added'}))
+                    .catch(error => res.status(401).json({error}));
             }
         })
 }
