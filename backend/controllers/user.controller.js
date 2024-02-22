b0a87672d539fef1c88615a6e3c024e2c1405392
Alexis Thierry <alexist103@gmail.com>
2024-02-22 08:28:27 +0100

    [FEATURE] adding user methods

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,23 +1,52 @@
 const User = require('../models/user.model');
-const jwt = require('jsonwebtoken');
+const validator = require('validator');
 const bcrypt = require('bcrypt');
 
 exports.getUsers = (req, res) => {
     User.find()
-        .then(users => res.status(200).json(users))
-        .catch(error => res.status(400).json({ error }))
+        .then(users => res.status(201).json(users))
+        .catch(error => res.status(401).json({ error }))
+}
+
+
+exports.checkUserCredential = (req, res) => {
+    User.findOne({email: req.body.email})
+        .then(user => {
+            if (user) {
+                bcrypt.compare(req.body.password, user.password)
+                    .then(valid => {
+                        if (valid) {
+                            res.status(201).json(true);
+                        } else {
+                            res.status(201).json(false);
+                        }
+                    })
+            } else {
+                res.status(201).json(false);
+            }
+        })
+        .catch(error => res.status(401).json({error}));
 }
 
 exports.addUser = (req, res) => {
-    const user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: bcrypt.hash(req.body.password, 10),
-        profile_picture: req.body.profile_picture
-    });
+    bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+            if (!validator.isEmail(req.body.email)) {
+                res.status(401).json({message: "provided_email_is_not_valid"})
+            } else {
+                const user = new User({
+                    username: req.body.username,
+                    email: req.body.email,
+                    password: hash,
+                    profile_picture: req.body.profile_picture
+                });
 
-    user.save()
-        .then(() => res.status(201).json({ message: 'user_added'}))
-        .catch(error => res.status(400).json({ error }));
+                user.save()
+                    .then(() => res.status(201).json({ message: 'user_added'}))
+                    .catch(error => res.status(401).json({ error }));
+            }
+        })
 }
+
+
 
