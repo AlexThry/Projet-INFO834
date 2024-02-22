fd2c05e42a2937ea784bebbb5936479c77f12b7b
Alexis Thierry <alexist103@gmail.com>
2024-02-22 08:29:05 +0100

    [FEATURE] adding messages methods

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,9 +1,10 @@
 const Message = require('../models/message.model');
+const mongoose = require("mongoose");
 
 exports.getMessages = (req, res) => {
     Message.find()
         .then(messages => res.status(201).json(messages))
-        .catch(error => res.status(400).json({error}));
+        .catch(error => res.status(401).json({error}));
 }
 
 exports.getMessagesSortedOffsetValues = (req, res) => {
@@ -11,9 +12,27 @@ exports.getMessagesSortedOffsetValues = (req, res) => {
         .sort({timestamp: -1})
         .skip(req.params.offset)
         .limit(req.params.values)
-        .then(messages => res.status(200).json(messages))
-        .catch(error => res.status(400).json({error}))
+        .then(messages => res.status(201).json(messages))
+        .catch(error => res.status(401).json({error}));
 }
+
+// récupérer les messages dans la conversation entre user1 et user2
+
+exports.getMessagesSortedByUserIdsOffsetValues = (req, res, next) => {
+    const user1 = new mongoose.Types.ObjectId(req.params.user1);
+    const user2 = new mongoose.Types.ObjectId(req.params.user2);
+    Message.find({
+        $or: [
+            {sender_id: user1, receiver_id: user2},
+            {sender_id: user2, receiver_id: user1},
+        ],
+    })
+        .sort({timestamp: -1})
+        .skip(req.params.offset)
+        .limit(req.params.values)
+        .then((message) => res.status(200).json(message))
+        .catch((error) => res.status(401).json({error}));
+};
 
 exports.addMessage = (req, res) => {
     const message = new Message({
@@ -24,6 +43,6 @@ exports.addMessage = (req, res) => {
     });
 
     message.save()
-        .then(() => res.status(200).json({message: "message_added"}))
-        .catch(error => res.status(400).json({error}));
+        .then(() => res.status(201).json({message: "message_added"}))
+        .catch(error => res.status(401).json({error}));
 }
