96a701509db90189ed2ca018897df5afbf4bd1f5
Alexis Thierry <alexist103@gmail.com>
2024-03-13 16:14:08 +0100

    [FEATURE] removing message list in chatroom

--- backend/models/chatroom.model.js
+++ backend/models/chatroom.model.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const chatRoomSchema = mongoose.Schema({
     user1: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     user2: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    messages: [{type: mongoose.Schema.Types.ObjectId, ref: 'Message'}]
 });
 
 module.exports = mongoose.model('ChatRoom', chatRoomSchema);
