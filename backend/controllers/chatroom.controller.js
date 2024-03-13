04775e427169e617aebb523064dc37135dc0a146
Alexis Thierry <alexist103@gmail.com>
2024-03-13 16:13:06 +0100

    [FEATURE] adding new chatroom

--- backend/controllers/chatroom.controller.js
+++ backend/controllers/chatroom.controller.js
@@ -1,6 +1,20 @@
 const ChatRoom = require('../models/chatroom.model');
 const mongoose = require("mongoose");
 
+exports.newChatRoom = (req, res) => {
+    const user1 = new mongoose.Schema.Types.ObjectId(req.body.user1_id);
+    const user2 = new mongoose.Schema.Types.ObjectId(req.body.user2_id);
+
+    const chatRoom = new ChatRoom({
+        user1: user1,
+        user2: user2
+    });
+
+    chatRoom.save()
+        .then(() => res.status(200).json({message: "chatroom_added"}))
+        .catch(error => res.status(400).json({error}))
+}
+
 exports.getChatByUsers = (req, res) => {
     const userId = new mongoose.Schema.Types.ObjectId(req.body.user_id);
     ChatRoom.find({
