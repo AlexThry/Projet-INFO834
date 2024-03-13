4f7451868120ed071b0f2797fd3c57aeccb02ff9
Alexis Thierry <alexist103@gmail.com>
2024-03-13 16:01:44 +0100

    [FEATURE] adding chatroom id

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const messageSchema = mongoose.Schema({
     sender_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     receiver_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    chatroom_id: { type: mongoose.Schema.Types.ObjectId, ref: 'ChatRoom', required: true },
     content: { type: String, required: true },
     timestamp: { type: Date, required: true }
 });
